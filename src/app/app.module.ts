c82abae55e04ce16ff9fccbdd993f15b5567c841
kimno24 <kikikikkik@gmail.com>
2020-02-01 00:24:51 -0500

    2 files modified, 5 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ItemDetailComponent } from './item-detail/item-detail.component';
-import { ClickDirective } from './click.directive';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ItemDetailComponent,
-    ClickDirective
+    AppComponent
   ],
   imports: [
     BrowserModule
